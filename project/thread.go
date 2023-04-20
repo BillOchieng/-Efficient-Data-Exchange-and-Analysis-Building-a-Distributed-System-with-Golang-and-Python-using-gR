8d3dc898c1536754f1b085b8fe30e0604fdf5cc4
BillOchieng <ochieng01@allegheny.edu>
2023-04-20 17:13:26 -0400

    add a go request

--- project/thread.go
+++ project/thread.go
@@ -6,25 +6,20 @@ import (
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	// This is what the function will print.
-	fmt.Fprintf(w, "Welcome to Educative Home!")
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bill is cool 😎 !")
 }
 
-func returnContact(w http.ResponseWriter, r *http.Request) {
-	// This is what the function will print.
+func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Email: support@educative.io")
 }
 
-func handleReq() {
-	// will call Home function by default.
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/contact", returnContact)
-
+func handleRequests() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/contact", contact)
 	log.Fatal(http.ListenAndServe(":8200", nil))
 }
 
 func main() {
-	// starting the API
-	handleReq()
+	handleRequests()
 }
