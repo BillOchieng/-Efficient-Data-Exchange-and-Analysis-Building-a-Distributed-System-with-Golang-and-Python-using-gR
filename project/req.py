854c0510f8590fc1e3ddfa832074bf47b04b5556
BillOchieng <ochieng01@allegheny.edu>
2023-04-20 17:40:22 -0400

    add python code

--- project/req.py
+++ project/req.py
@@ -1,59 +1,14 @@
 import requests
-import json
-import cProfile
-import pandas as pd
-import matplotlib.pyplot as plt
 
-# I use try and except blocks to handle the JSONDecodeError exception that could occur if the response from the server is not a valid JSON string.
-# If the exception is raised, I print an error message and set the data variable to an empty dictionary.
-# This way, the rest of the code can still run without errors, even if there was a problem with the data received from the server.
-try:
-    response = requests.get("https://api.warframe.market/v1/items/")
-    data = json.loads(response.text)
-except json.JSONDecodeError as e:
-    print(f"Error decoding JSON: {e}")
-    data = {}
+# Send an HTTP GET request to the '/' endpoint in the Go code
+response = requests.get('http://localhost:8200/')
+print(response.text)
 
-# Perform data analysis on data
+# Send an HTTP GET request to the '/contact' endpoint in the Go code
+response = requests.get('http://localhost:8200/contact')
+print(response.text)
 
-# I use the `get` method from the `requests` library to send a GET request to the Golang server running on `localhost:8080`.
-# We then use the `json` method to decode the JSON data received from the server.
-
-# Once I have received the data in Python, I can use libraries like NumPy, Pandas, and Matplotlib to perform data analysis and visualization.
-
-counter = 0
-counter_data = []
-
-
-def increment():
-    global counter
-    for i in range(1000000):
-        counter += 1
-    counter_data.append(counter)
-
-
-def decrement():
-    global counter
-    for i in range(1000000):
-        counter -= 1
-    counter_data.append(counter)
-
-
-cProfile.run("increment(); decrement()")
-
-df = pd.DataFrame(counter_data, columns=["counter"])
-df.to_csv("counter_data.csv", index=False)
-
-mean = df.mean()["counter"]
-std = df.std()["counter"]
-max_val = df.max()["counter"]
-
-print(f"Mean value of counter: {mean}")
-print(f"Standard deviation of counter: {std}")
-print(f"Maximum value of counter: {max_val}")
-
-plt.plot(counter_data)
-plt.xlabel("Time")
-plt.ylabel("Counter Value")
-plt.title("Counter Value over Time")
-plt.show()
+# Send an HTTP POST request to the '/submit' endpoint in the Go code
+data = {'name': 'John Doe', 'email': 'johndoe@example.com'}
+response = requests.post('http://localhost:8200/submit', data=data)
+print(response.text)
